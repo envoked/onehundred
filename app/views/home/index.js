486b08147024cbf78faf0574a80d7ab00b240fec
Adrian Wisernig <adrian.wisernig@envoked.com>
2013-08-11 16:30:31 -0700

    Experiments

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,6 +1,10 @@
 var BaseView = require('../base');
 
 module.exports = BaseView.extend({
-  className: 'home_index_view'
+  className: 'home_index_view',
+
+  postRender: function(){
+  	console.log("Hello");
+  }
 });
 module.exports.id = 'home/index';
