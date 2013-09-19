67e63b332fbb2ebdf3ec097019130f98bc18801a
Adrian Wisernig <adrian.wisernig@envoked.com>
2013-09-18 22:59:24 -0700

    re-add background video

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -12,6 +12,7 @@ module.exports = BaseView.extend({
 
   postRender: function(){
     this.initScroll();
+    this.startVideo();
   },
 
   initScroll: function(){
@@ -22,8 +23,26 @@ module.exports = BaseView.extend({
 
     controller.addTween('#nostalgia',
       TweenMax.from($('#nostalgia'), 0.5, {css:{opacity:0},
-      onComplete: function() { }}, 800)
+      onComplete: function() { self.playVideo();}}, 800)
     );
+
   },
+
+  playVideo: function(){
+    $('#nostalgia .videoBG').addClass('focused');
+    $('#nostalgia video')[0].play();
+  },
+
+  startVideo: function(){
+      $('#nostalgia').videoBG({
+      mp4    : '/videos/nostalgia/nostalgia.mp4',
+      webm   : '/videos/nostalgia/nostalgia.webm',
+      ogv    : '/videos/nostalgia/nostalgia.ogg',
+      poster : '/videos/nostalgia/nostalgia.jpg',
+      scale  : true,
+      zIndex : 0,
+    });
+    $('#nostalgia video')[0].pause();
+  }
 });
 module.exports.id = 'home/index';
